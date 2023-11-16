4fb1812d42fbc354ed674752e77d535bd3e9ed17
devcarkson <decarkson@gmail.com>
2023-11-16 13:56:33 +0100

    initial commit

---
+++ src/main.jsx
@@ -0,0 +1,36 @@
+// import React from 'react'
+// import ReactDOM from 'react-dom/client'
+// import { PersistGate } from "redux-persist/integration/react";
+// import { Provider } from "react-redux";
+// import { store, persistor } from "./redux/store";
+// import App from './App.jsx'
+// import './index.css'
+
+// ReactDOM.createRoot(document.getElementById('root')).render(
+//   <React.StrictMode>
+//     <Provider store={store}>
+//       <PersistGate loading={null} persistor={persistor}>
+//         <App />
+//       </PersistGate>
+//   </Provider>
+//   </React.StrictMode>,
+// )
+
+
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { PersistGate } from "redux-persist/integration/react";
+import "slick-carousel/slick/slick.css";
+import { Provider } from "react-redux";
+import { store, persistor } from "./redux/store";
+import "./index.css";
+import App from "./App";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <Provider store={store}>
+    <PersistGate loading={null} persistor={persistor}>
+      <App />
+    </PersistGate>
+  </Provider>
+);
